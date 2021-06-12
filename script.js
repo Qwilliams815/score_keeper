e9999fd36b61403d9ffa0213ecd99011d8670378
Qwilliams815 <68478526+Qwilliams815@users.noreply.github.com>
2021-06-11 20:54:11 -0700

    Fully Functional edition, few tweaks left on TODO

--- script.js
+++ script.js
@@ -24,10 +24,51 @@ function resetScore() {
     updateBottomDisplay(bottomScore);
 }
 
+
+var colorWindow = document.querySelector('.color-window');
+
 function showColors() {
-    alert("YOOOOO")
+    //alert("YOOOOO");
+    colorWindow.classList.add('color-window-active')
+};
+
+function exit() {
+    colorWindow.classList.remove('color-window-active')
+}
+
+//Color Changing Functions
+
+function colorChangeTopRed() {
+    let topBox = document.getElementById("top-box")
+    topBox.style.backgroundColor = "#ed3430";
+}
+
+function colorChangeTopGreen() {
+    let topBox = document.getElementById("top-box")
+    topBox.style.backgroundColor = "#01c756";
+}
+
+function colorChangeTopBlue() {
+    let topBox = document.getElementById("top-box")
+    topBox.style.backgroundColor = "#154478";
+}
+
+function colorChangeBottomRed() {
+    let bottomBox = document.getElementById("bottom-box")
+    bottomBox.style.backgroundColor = "#ed3430";
+}
+
+function colorChangeBottomGreen() {
+    let bottomBox = document.getElementById("bottom-box")
+    bottomBox.style.backgroundColor = "#01c756";
+}
+
+function colorChangeBottomBlue() {
+    let bottomBox = document.getElementById("bottom-box")
+    bottomBox.style.backgroundColor = "#154478";
 }
 
 function test() {
     alert("hello")
 }
+
