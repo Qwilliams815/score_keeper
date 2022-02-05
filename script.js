d267c98033529bdce5811336ff4247ec7c0038b7
Brian Williams <brianman815@gmail.com>
2022-02-04 22:11:12 -0800

    Added PWA support

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+     navigator.serviceWorker.register('../sw.js').then( () => {
+      console.log('Service Worker Registered')
+     })
+   })
+  }
+
 
 let topScore = 0;
 let bottomScore = 0;
